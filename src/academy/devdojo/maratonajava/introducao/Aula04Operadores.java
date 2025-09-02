3e1a1330efec4ba5d0b4ce1c326c9490e4c9d86a
Guilherme  Emiliano <guiemiliano.dev@gmail.com>
2025-09-01 21:10:12 -0300

    Operadores

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -5,7 +5,8 @@ public class Aula04Operadores {
         // + - / *
         int numero1 = 10;
         int numero2 = 20;
-        int soma = numero1 + numero2;
+        int resultado = numero1 * numero2;
+        System.out.println(resultado);
 
 
     }
