c72307fefdba70fa533c85dd3d6c5d1019db4569
Guilherme  Emiliano <guiemiliano.dev@gmail.com>
2025-09-02 23:14:12 -0300

    Operadores

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -18,5 +18,22 @@ public class Aula04Operadores {
         System.out.println("10 é diferente de 20? "+ isDezMaiorQueVinte);
 
         //Operadores lógicos
+        //&& (AND E)   || (OR OU)  ! (NOT NÃO)
+        int idade = 29;
+        double salario = 3500.0;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println(isDentroDaLeiMaiorQueTrinta);
+        System.out.println(isDentroDaLeiMenorQueTrinta);
+
+        double currentAccount = 290;
+        double savingsAccount = 10000;
+        double playstationFivePrice = 5000;
+        boolean isPlaystationFiveCanBeBought = currentAccount >= playstationFivePrice || savingsAccount >= playstationFivePrice;
+
+        System.out.println("Playstation five can be bought? " + isPlaystationFiveCanBeBought);
+
+
     }
 }
