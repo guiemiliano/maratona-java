6191d9e598ee304f107395b101ac25c057e5a219
Guilherme  Emiliano <guiemiliano.dev@gmail.com>
2025-09-10 10:53:01 -0300

    reforçando sobre arrays

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -2,6 +2,14 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays02 {
     public static void main(String[] args) {
+        String[] nomes = new String[3];
+        nomes[0] = "Darius";
+        nomes[1] = "Draven";
+        nomes[2] = "Swain";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
 
     }
 }
