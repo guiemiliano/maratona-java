d18843e87000cd2052451fa060570e42432ea11f
Guilherme  Emiliano <guiemiliano.dev@gmail.com>
2025-09-10 11:22:05 -0300

    reforçando sobre arrays

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -5,5 +5,9 @@ public class Aula07Arrays03 {
         int[] numeros = new int[3];
         int[] numeros2 = {1,2,3,4,5};
 
+        for (int i = 0; i < numeros2.length; i++) {
+            System.out.println(numeros2[i]);
+        }
+
     }
 }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays04.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays04 {
+    public static void main(String[] args) {
+        //foreach, ele não da indices
+
+        int numeros[] = {1,2,3,4,5};
+        for (int num: numeros){
+            System.out.println(num);
+        }
+    }
+}
